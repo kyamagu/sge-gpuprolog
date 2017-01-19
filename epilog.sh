f266368f69444cd24b0a6039afa213a9cfd38e76
GeertGeurts <geert@verweggistan.eu>
2017-01-19 11:06:29 +0100

    Add files via upload
    1) Epilog released locks via lockfiles named /tmp/lock-nvidia[0-9] where the prolog used /tmp/lock-gpu[0-9]. efectively never releasing locks.
    
    2) The gpus that got locked by a failed prolog run never got released.
    
    3) Some speed improvents.

--- epilog.sh
+++ epilog.sh
@@ -3,7 +3,7 @@
 # Finish script to release GPU devices.
 #
 # Kota Yamaguchi 2015 <kyamagu@vision.is.tohoku.ac.jp>
-
+# Geert Geurts 2017 <geert.geurts@dalco.ch>
 # Check if the environment file is readable.
 ENV_FILE=$SGE_JOB_SPOOL_DIR/environment
 if [ ! -f $ENV_FILE -o ! -r $ENV_FILE ]
@@ -12,16 +12,13 @@ then
 fi
 
 # Remove lock files.
-device_ids=$(grep SGE_GPU $ENV_FILE | \
-             sed -e "s/,/ /g" | \
-             sed -n "s/SGE_GPU=\(.*\)/\1/p" | \
-             xargs shuf -e)
+device_ids=$(awk -F "=" '{if ($1=="SGE_GPU") print $2}' $ENV_FILE|tr "," " ")
 for device_id in $device_ids
 do
-  lockfile=/tmp/lock-nvidia$device_id
+  lockfile=/tmp/lock-gpu$device_id
   if [ -d $lockfile ]
   then
-    rmdir -f $lockfile
+    rmdir $lockfile
   fi
 done
 exit 0
